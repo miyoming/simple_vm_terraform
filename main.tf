023893fd70b2ed137096c70883e50974f3566065
miyoming12 <romeo.nagajr@gmail.com>
2026-08-24 11:52:24 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+
+variable "prefix" {
+  default = "tfvmex"
+}
+
+
+data "azurerm_virtual_network" "existing" {
+  name                = "miyoming_vnet"
+  resource_group_name = "vnet"
+}
+
+data "azurerm_subnet" "existing_subnet" {
+  name                 = "default"
+  virtual_network_name = data.azurerm_virtual_network.existing.name
+  resource_group_name  = data.azurerm_virtual_network.existing.resource_group_name
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "southeastasia"
+}
+
+
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = data.azurerm_virtual_network.existing.location
+  resource_group_name = data.azurerm_virtual_network.existing.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = data.azurerm_subnet.existing_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B1s"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-miyoming"
+    admin_username = "miyoming"
+    admin_password = "P@ssw0rd1dc123"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+#  tags = {
+#    environment = "staging"
+#  }
+}
