ef0d9ca56bd5ab8e18605aaaf90bda92da2077a3
miyoming12 <romeo.nagajr@gmail.com>
2026-08-31 17:31:12 +0800

    romeo

--- main.tf
+++ main.tf
@@ -41,6 +41,36 @@ resource "azurerm_resource_group" "example" {
   location = "southeastasia"
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  dynamic "security_rule" {
+    for_each = var.nsg_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+    source_port_range          = security_rule.value.source_port_range
+    destination_port_range     = security_rule.value.destination_port_range
+    source_address_prefix      = security_rule.value.source_address_prefix
+    destination_address_prefix = security_rule.value.destination_address_prefix
+    }
+  }
+
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.existing_subnet.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+
 
 
 resource "azurerm_public_ip" "example" {
@@ -64,6 +94,8 @@ resource "azurerm_network_interface" "main" {
     public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
+
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -3,5 +3,24 @@ variable "admin_password" {
   type        = string
 }
 
+variable "nsg_name" {
+  description = "The name of the network security group"
+  type        = string
+  
+}
 
 
+variable "nsg_rules" {
+  description = "A list of network security group rules"
+  type = list(object({
+    name      = string
+    priority  = number
+    direction = string
+    access    = string
+    protocol  = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+}
